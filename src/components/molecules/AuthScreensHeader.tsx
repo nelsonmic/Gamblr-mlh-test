b4b31e57406dc73754c35ef061cf4ecae3722570
nelsonmic <iamnelsonmichael@gmail.com>
2023-11-25 19:38:58 +0100

    fix(auth screen): minor fix

--- src/components/molecules/AuthScreensHeader.tsx
+++ src/components/molecules/AuthScreensHeader.tsx
@@ -6,12 +6,25 @@ type AuthScreensHeaderProps = {
       title: string;
       description: string;
       style?: string;
+      textsPosition?: "left" | "center" | "right"
 }
-export const AuthScreenHeader: FC<AuthScreensHeaderProps> = ({title, description, style}) => {
+export const AuthScreenHeader: FC<AuthScreensHeaderProps> = ({title, description, style, textsPosition ="left"}) => {
       return (
-            <View className={clsx("space-y-2 pr-8", style)}>
-                  <Text className="text-3xl font-[700] leading-8 tracking-[0.26px] text-black-100 font-cabinetGroteskBold">{ title }</Text>
-                  <Text className="font-interRegular text-black-100 text-sm leading-4">{ description }</Text>
+            <View className={clsx("space-y-2", style)}>
+                  <Text className={clsx("text-3xl font-[700] leading-8 tracking-[0.26px] text-black-100 font-cabinetGroteskBold",
+                        {
+                              "text-center" : textsPosition === "center",
+                              "text-left" : textsPosition === "left",
+                              "text-right" : textsPosition === "right"
+                        }
+                  )}>{ title }</Text>
+                  <Text className={clsx("font-interRegular text-black-100 text-sm leading-4",
+                        {
+                              "text-center" : textsPosition === "center",
+                              "text-left" : textsPosition === "left",
+                              "text-right" : textsPosition === "right"
+                        }
+                  )}>{ description }</Text>
             </View>
       )
 }
